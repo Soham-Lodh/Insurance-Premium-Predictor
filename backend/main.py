59d7176021bc3fb7e161127a9034af6ac0817341
Soham-Lodh <sohamlodh06@gmail.com>
2026-04-23 13:46:07 +0530

    updates

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,10 @@ app = FastAPI(title="Insurance Premium Predictor API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://insurance-premium-predictor-drab.vercel.app"],
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
